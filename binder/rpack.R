e9c6e1b34e7c6bde03b45f969eee8a539cfc9640
Serhat Cevikel <serhatcevikel@yahoo.com>
2021-02-08 21:31:50 +0300

    initial commit

---
+++ binder/rpack.R
@@ -0,0 +1,16 @@
+    cranlist <- c(
+                  "Quandl",
+                  "ecb",
+                  "eurostat"
+    )
+
+
+    ## cran packages
+    for (package in cranlist)
+    { 
+        if (!require(package, character.only = T, quietly = T))
+        {
+            install.packages(package)
+        }
+    }
+
